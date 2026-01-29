53fbc9871e7cb5b99d81a011852fb191eecd29c6
davidson124 <daferdinat@gmail.com>
2026-01-29 13:50:55 -0500

    feat: MongoDB connection with mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import dbConection from './config/mongo.js';
+
 const app = express();
+
+// Connect to MongoDB
+dbConection();
+
 // Middleware to read JSON
 app.use(express.json());
 // Rute base of testing
